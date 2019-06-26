5eb28ae484ad0f8def26f6d6e858c7af68deff61
bitlucky <lokeshtiwari877@gmail.com>
2019-06-26 17:40:33 +0530

    understand require modules

--- 2. Basic FUndamentals/1. Understand Require/app.js
+++ 2. Basic FUndamentals/1. Understand Require/app.js
@@ -3,6 +3,8 @@
 //Fs module 
 const fs = require('fs');
 
+const index = require('./index.js');  // this will import index.js file in this file
+
 
 //First Method to use appendFile 
 fs.appendFile('message.txt', 'Hello this is my first file!')
@@ -24,3 +26,8 @@ const os = require('os')
 var userinfo = os.userinfo();
 console.log("hello" + userinfo );
 console.log(`hello ${userinfo} !` ); // ES6 template string 
+
+
+// using the required Index.js file
+var res = index.add(5,6);
+console.log(res);
