ed0c322945c4baf8d2599cf47963e1f0d79b8a5e
bitlucky <lokeshtiwari877@gmail.com>
2019-07-10 13:45:48 +0530

    Advanced Templates

--- 4. Web_Servers_and_Application_deployment/4. advanced-templates/server.js
+++ 4. Web_Servers_and_Application_deployment/4. advanced-templates/server.js
@@ -2,31 +2,33 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine' , 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+      return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt', (text) => {
+hbs.registerHelper('screamIt' , (text) =>{
   return text.toUpperCase();
-});
+})
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+  res.render('home.hbs' , {
+    pageTitle : 'Home page',
+    welcomeMessage : "Welcome Reflex to the Relfexian army ",
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
+  res.render('about.hbs',{
+    pageTitle : 'About Page',
+    currentYear : new Date().getFullYear()
   });
 });
+
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
