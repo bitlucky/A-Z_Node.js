05f90a51b655f64ca8ed8a13df585e136154b0bf
bitlucky <lokeshtiwari877@gmail.com>
2019-07-10 14:22:02 +0530

    adding middleware

--- 4. Web_Servers_and_Application_deployment/5. middleware/server.js
+++ 4. Web_Servers_and_Application_deployment/5. middleware/server.js
@@ -1,47 +1,55 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine' , 'hbs');
+
+
+app.use((req,res,next)=>{
   var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-
+  var log = `${now} :  ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log' , log + '\n', (err)=>{
+    if(err){
+      console.log('Unable to log' );
+    }
+  });
   next();
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+      return new Date().getFullYear()
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
+hbs.registerHelper('screamIt' , (text) =>{
+  return text.toUpperCase();
+})
+app.use((req, res , next )=>{
+  res.render('maintenance.hbs');
+
+});
 
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
-
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+  res.render('home.hbs' , {
+    pageTitle : 'Home page',
+    welcomeMessage : "Welcome Reflex to the Relfexian army ",
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
+  res.render('about.hbs',{
+    pageTitle : 'About Page',
+    currentYear : new Date().getFullYear()
   });
 });
+
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
