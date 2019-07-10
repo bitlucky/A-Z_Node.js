a7d499c1584293a12938c65b43caa7c8f5ce7954
bitlucky <lokeshtiwari877@gmail.com>
2019-07-10 10:52:06 +0530

    Rendering Tempelates using HBS

--- 4. Web_Servers_and_Application_deployment/3. rendering-templates/server.js
+++ 4. Web_Servers_and_Application_deployment/3. rendering-templates/server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-
-app.set('view engine', 'hbs');
+ 
+app.set('view engine' , 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
+  res.render('home.hbs' , {
+    pageTitle : 'Home page',
+    welcomeMessage : "Welcome Reflex to the Relfexian army ",
     currentYear: new Date().getFullYear()
-  });
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+  res.render('about.hbs',{
+    pageTitle : 'About Page',
+    currentYear : new Date().getFullYear()
   });
 });
 
